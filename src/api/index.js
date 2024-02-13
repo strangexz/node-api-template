588d555d07dcc45951710373920d612749f7337c
Juan Alberto Figueroa Lagos <juan.figueroa@tigo.com.hn>
2024-02-13 16:08:19 -0600

    build: agregada estructura base de carpetas

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const {v1} = require('uuid');
+const expressHttpContext = require('express-http-context');
