1243cde579efc0d2bf3289abba80e2dab425f203
Juan Alberto Figueroa Lagos <juan.figueroa@tigo.com.hn>
2024-02-16 16:57:40 -0600

    feat: nuevos metodos utilitarios agregados

--- src/utils/utillities.js
+++ src/utils/utillities.js
@@ -1,3 +1,6 @@
+const _ = require('underscore');
+
+
 /**
  * Devuelve fecha ISO formateada
  *
@@ -19,4 +22,72 @@ const formatedIsoStringDate = (isoStringDate) => isoStringDate.replace(/T/, ' ')
  */
 const formatedTimestamp = () => new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
-module.exports = { formatedIsoStringDate, formatedTimestamp };
+/**
+ * Cambia Camel Case a Snake Case
+ *
+ * Obtiene una cade de caracteres en camel case y devuelve otra cadena de caracteres
+ * transformada a snake case
+ *
+ * @param {String} str - Cadena de caracteres en camel case
+ * @returns Devuelve una cadena de caracteres en snake case
+ */
+const camelToSnakeCase = (str) => str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+
+/**
+ * Contador de atributos
+ *
+ * Esta función se encarga de contar la cantidad de atributos de un modelo,
+ * validando si este tien ciertos atributos especiales
+ *
+ * @alias countAttributes
+ * @param {Object} attributes - arreglo de atributos del modelo
+ * @param {Boolean} hasUUID - tiene un UUID?
+ * @param {Boolean} hasCreatedAt - tiene una fecha de creación?
+ * @param {Boolean} hasUpdatedAt - tiene una fecha de actualización?
+ * @returns {Object} devuelve un objeto con la cantidad total de atributos y un arreglo con sus llaves
+ */
+const countAttributes = (attributes, hasUUID, hasCreatedAt, hasUpdatedAt) => {
+  let attributesCount = Object.keys(attributes).length;
+  let attributesNames = Object.keys(attributes);
+  const attributesColumnNames = [];
+
+  for (const key in attributes) {
+    if (Object.hasOwnProperty.call(attributes, key)) {
+      const element = attributes[key];
+
+      if (_.isUndefined(element.columnName)) {
+        attributesNames = _.without(attributesNames, key);
+        attributesCount -= 1;
+      } else if (element.aRelationMap === true) {
+        attributesNames = _.without(attributesNames, key);
+        attributesCount -= 1;
+      } else {
+        attributesColumnNames.push(element.columnName);
+      }
+    }
+  }
+
+  // si el ID es un UUID ya viene en el conteo de atributos del modelo
+  // si no lo es es autoincremental y debe agregarse
+  if (hasUUID === false) {
+    attributesCount += 1;
+    attributesNames.push('id');
+    attributesColumnNames.push('id');
+  }
+
+  if (hasCreatedAt === true) {
+    attributesCount += 1;
+    attributesNames.push('created_at');
+    attributesColumnNames.push('created_at');
+  }
+
+  if (hasUpdatedAt === true) {
+    attributesCount += 1;
+    attributesNames.push('updated_at');
+    attributesColumnNames.push('updated_at');
+  }
+
+  return { attributesCount, attributesNames, attributesColumnNames };
+};
+
+module.exports = { formatedIsoStringDate, formatedTimestamp, camelToSnakeCase, countAttributes };
