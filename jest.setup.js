e5285b5acf3599955f0cf3f0ab16aa319da005b1
Juan Alberto Figueroa Lagos <juan.figueroa@tigo.com.hn>
2024-02-22 18:28:32 -0600

    config: actualizados archivos de configuracion

--- jest.setup.js
+++ jest.setup.js
@@ -1,24 +1,22 @@
 const objection = require('objection');
-const configuration = require('../../../knexfile')['test'];
+const configuration = require('./knexfile')['test'];
 const knex = require('knex')(configuration);
 
 const { transaction, Model } = objection;
 
-global.beforeAll(async () => {
-  global.knex = knex;
-  global.txn = null;
-});
+beforeAll(async () => {
+  await knex.migrate
+    .latest()
+    .then(() => {
+      jest.useRealTimers();
+      return;
+    })
+    .then(() => {
+      // migrations are finished
+      console.info('Seeder finished!');
+    });
+}, 60000);
 
-global.beforeEach(async () => {
-  global.txn = await transaction.start(knex);
-  Model.knex(global.txn);
-});
-
-global.afterEach(async () => {
-  await global.txn.rollback();
-  Model.knex(knex);
-});
-
-global.afterAll(async () => {
-  global.knex.destroy();
-});
+afterAll(() => {
+  knex.destroy();
+}, 60000);
