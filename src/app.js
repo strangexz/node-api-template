0c8251c67f8ca00c2c49b266842782f1b0fe3e4d
Strangexz <ja_figueroa@outlook.es>
2024-08-29 11:15:20 -0600

    chore: mensaje de arranque modificado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const envs = require('./config/enviroments');
 const Group = require('../src/models/ParamGroup');
 
 api.listen(envs.portServer, async () => {
-  log.info('Servidor corriendo en el puerto 3000');
+  log.info(`Servidor corriendo en el puerto ${envs.portServer}`);
   const group = await Group.query();
   console.log(group);
   const newGroup = await Group.query().insertGraphAndFetch({ name: 'grupo de prueba', isSpecial: false });
